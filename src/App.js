e9ef59f3088e475ea9ab60f50f73e00715cd9198
satyamrkushwaha <satyamkushwahasrk95@gmail.com>
2024-05-27 00:32:46 +0530

    removed import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import IpoList from './ipoList';
 import IpoDetails from './ipoDetails';
